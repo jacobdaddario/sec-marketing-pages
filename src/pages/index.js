cb64312571c68520239b058e15d0da1bfcc3b302
Jacob Daddario <jake.daddario@gmail.com>
2020-08-29 17:55:13 -0500

    Added an image gallery to the splash

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,35 +5,52 @@ import Img from "gatsby-image";
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query Images {
-      image: file(relativePath: {eq: "sec_logo_black.png"})
-      {
-        id
-        childImageSharp {
-          fixed {
-            ...GatsbyImageSharpFixed
+      images: allFile(filter: { relativeDirectory: { eq: "landing" } }) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
       }
     }
   `);
 
+  console.log(data);
+
   return (
     <div className="flex flex-col items-center w-full bg-white">
-      <div className="max-w-4xl w-full">
-        <div className="flex flex-row">
-          <div>
-            <h1 className="mx-auto mt-32">Name</h1>
+      <div className="max-w-6xl w-full">
+        <div className="flex flex-row mt-32">
+          <div className="w-1/2">
+            <h1 className="mx-auto text-2xl">
+              Join the <span className="text-maroon-500">blah blah blah</span>
+            </h1>
           </div>
-          <div>
+          <div className="grid grid-cols-4 gap-8 w-3/5">
             <Img
-              fixed={data.image.childImageSharp.fixed}
-              alt="sec logo"
+              fluid={data.images.nodes[0].childImageSharp.fluid}
+              className="col-start-2 col-span-3 h-40 bg-gray-600 border border-gray-300 rounded-lg shadow-xl"
+            />
+            <Img
+              fluid={data.images.nodes[1].childImageSharp.fluid}
+              className="col-span-2 h-40 bg-gray-600 border border-gray-300 rounded-lg shadow-xl"
+            />
+            <Img
+              fluid={data.images.nodes[2].childImageSharp.fluid}
+              className="col-span-2 h-40 bg-gray-600 border border-gray-300 rounded-lg shadow-xl"
+            />
+            <Img
+              fluid={data.images.nodes[3].childImageSharp.fluid}
+              className="col-span-3 h-40 bg-gray-600 border border-gray-300 rounded-lg shadow-xl"
             />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default IndexPage;
