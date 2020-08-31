2191ff12ac63c13d5598e15db763f46c66f72a9e
Jacob Daddario <jake.daddario@gmail.com>
2020-08-31 02:30:51 -0500

    Added a navbar to the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Layout from "../components/Layout";
 import HeroSlideShow from "../components/HeroSlideShow";
 import HeroText from "../components/HeroText";
 
 const IndexPage = () => (
-  <div className="flex flex-col items-center w-full bg-white">
-    <div className="max-w-6xl w-full">
-      <div className="flex flex-row h-140 mt-20">
-        <HeroText />
-        <HeroSlideShow />
+  <Layout>
+    <div className="flex flex-col items-center w-full bg-white">
+      <div className="max-w-6xl w-full">
+        <div className="flex flex-row h-140 mt-12">
+          <HeroText />
+          <HeroSlideShow />
+        </div>
       </div>
     </div>
-  </div>
+  </Layout>
 );
 
 export default IndexPage;
